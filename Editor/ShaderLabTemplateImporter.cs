87aa7d33ac330647aa2130d7f2fdf0b44f516172
tanitta <ttata.trit@gmail.com>
2024-10-03 16:00:21 +0900

    Enable to detect global variables.

--- Editor/ShaderLabTemplateImporter.cs
+++ Editor/ShaderLabTemplateImporter.cs
@@ -5,6 +5,7 @@ using UnityEditor;
 using UnityEditor.AssetImporters;
 using System.IO;
 using Scriban;
+using Scriban.Syntax;
 
 namespace trit.ShaderLabTemplate{
     [ScriptedImporter(1, "shadertmp")]
@@ -27,9 +28,8 @@ namespace trit.ShaderLabTemplate{
                 }
                 return;
             }
+
             var page = template.Page;
-
-
 
             var model = CreateModelFromProperties();
             string renderedShaderSource = template.Render(model);
@@ -46,6 +46,12 @@ namespace trit.ShaderLabTemplate{
             }
         }
 
+        public void SetTemplateVariables(){
+            
+            foreach(var v in _variables){
+            }
+        }
+
         private object CreateModelFromProperties()
         {
             var model = new Dictionary<string, object>();
@@ -55,6 +61,46 @@ namespace trit.ShaderLabTemplate{
             }
             return model;
         }
+        public List<string> GetTemplateVariables()
+        {
+            string assetPath = this.assetPath;
+            string fullPath = Path.GetFullPath(assetPath);
+            string shaderSource= File.ReadAllText(fullPath);
+            var template = Template.Parse(shaderSource);
+
+            if (template.HasErrors)
+            {
+                foreach (var message in template.Messages)
+                {
+                    Debug.LogError($"Template Error: {message}");
+                }
+                return null;
+            }
+
+            var collector = new VariableCollector();
+            collector.Visit(template.Page);
+
+            var context = new TemplateContext();
+            var builtInObject = context.BuiltinObject;
+
+            var builtInMembers = new HashSet<string>(builtInObject.GetMembers());
+            // var builtInMembers = new HashSet<string>(
+            //     builtInObject.GetMembers().Select(member => member.Key)
+            // );
+
+            var undefinedVariables = new List<string>();
+
+            foreach (var variable in collector.VariablesUsed)
+            {
+                bool cond = !collector.VariablesAssigned.Contains(variable) && !builtInMembers.Contains(variable);
+                if (cond)
+                {
+                    undefinedVariables.Add(variable);
+                }
+            }
+
+            return undefinedVariables;
+        }
     }
 
     [System.Serializable]
@@ -63,33 +109,48 @@ namespace trit.ShaderLabTemplate{
         public string name;
 
         public string stringValue;
-        public string valueType;
 
         public object GetValue()
         {
             return stringValue;
         }
-
-        public object GetValueType()
-        {
-            return valueType;
-        }
     }
 
-    public class VariableCollector : Scriban.Syntax.ScriptVisitor
+    public class VariableCollector : ScriptVisitor
+{
+    public HashSet<string> VariablesUsed { get; } = new HashSet<string>();
+    public HashSet<string> VariablesAssigned { get; } = new HashSet<string>();
+
+    public override void Visit(ScriptVariableGlobal node)
     {
-        public HashSet<string> Variables { get; } = new HashSet<string>();
-
-        public override void Visit(Scriban.Syntax.ScriptVariableGlobal node)
-        {
-            Variables.Add(node.Name);
-            base.Visit(node);
-        }
-
-        public override void Visit(Scriban.Syntax.ScriptVariableLocal node)
-        {
-            Variables.Add(node.Name);
-            base.Visit(node);
-        }
+        VariablesUsed.Add(node.Name);
+        base.Visit(node);
     }
+
+    // public override void Visit(ScriptVariableLocal node)
+    // {
+    //     VariablesUsed.Add(node.Name);
+    //     base.Visit(node);
+    // }
+
+    public override void Visit(ScriptAssignExpression node)
+    {
+        if (node.Target is ScriptVariable)
+        {
+            var variable = (ScriptVariable)node.Target;
+            VariablesAssigned.Add(variable.Name);
+        }
+        base.Visit(node);
+    }
+
+    // public override void Visit(ScriptFunction node)
+    // {
+    //     foreach (var parameter in node.Parameters)
+    //     {
+    //         // parameter は ScriptParameter 型
+    //         VariablesAssigned.Add(parameter.Name);
+    //     }
+    //     base.Visit(node);
+    // }
+}
 }
---
+++ Editor/ShaderLabTemplateImporterEditor.cs
@@ -0,0 +1,46 @@
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+using Scriban;
+using UnityEditor.AssetImporters;
+using Scriban.Syntax;
+
+namespace trit.ShaderLabTemplate
+{
+    [CustomEditor(typeof(ShaderLabTemplateImporter))]
+    public class ScribanShaderImporterEditor : ScriptedImporterEditor
+    {
+        public override void OnInspectorGUI()
+        {
+            var importer = target as ShaderLabTemplateImporter;
+
+            if(GUILayout.Button("Detect Variables",GUILayout.Width(240))){
+                var variables = importer.GetTemplateVariables();
+                foreach(var v in variables){
+                    Debug.Log(v);
+                }
+                SyncVariables(importer, variables);
+                EditorUtility.SetDirty(importer);
+            };
+            base.OnInspectorGUI();
+        }
+
+        private void SyncVariables(ShaderLabTemplateImporter importer, List<string> variableNames)
+        {
+            var existingVariables = importer._variables;
+
+            foreach (var variableName in variableNames)
+            {
+                if (!existingVariables.Exists(v => v.name == variableName))
+                {
+                    existingVariables.Add(new ShaderVariable() { name = variableName });
+                }
+            }
+
+            // Remove
+            // existingVariables.RemoveAll(v => !variableNames.Contains(v.name));
+        }
+    }
+
+}
