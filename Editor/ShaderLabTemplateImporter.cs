9cb5b8612402cb3d217a18e3c611d792298c4f33
tanitta <ttata.trit@gmail.com>
2024-11-07 21:18:43 +0900

    Add directive.

--- Editor/ShaderLabTemplateImporter.cs
+++ Editor/ShaderLabTemplateImporter.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -154,3 +155,4 @@ namespace trit.ShaderLabTemplate{
     // }
 }
 }
+#endif
--- Editor/ShaderLabTemplateImporterEditor.cs
+++ Editor/ShaderLabTemplateImporterEditor.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
 using System.IO;
@@ -44,3 +45,4 @@ namespace trit.ShaderLabTemplate
     }
 
 }
+#endif
